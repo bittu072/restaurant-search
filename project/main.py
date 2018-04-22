83e82f05e29812e52c5b6c5e74f0fbf4e43aa71e
bittu072 <bittu.patel072@gmail.com>
2018-04-21 19:43:20 -0700

    turned off the debug for the app

--- project/main.py
+++ project/main.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     # always turn off debugging
     # when the application moves to production environment.
-    app.debug = True
+    # app.debug = True
+    app.debug = False
     # defines which port to use
     port = int(os.environ.get("PORT", 5000))
     # defines to consider host as localhost
